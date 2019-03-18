a727b7b58999731edf83b1885ea3bb7071aba6c5
andr68rus <ashtmb@gmail.com>
2019-03-18 07:51:25 +0300

    omni-fy

--- omni_chiron.mk
+++ omni_chiron.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2010 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,21 +17,33 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
 # Inherit from chiron device
 $(call inherit-product, device/xiaomi/chiron/device.mk)
 
-# Inherit some common nitrogen stuff.
-$(call inherit-product, vendor/nitrogen/products/common.mk)
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_NAME := nitrogen_chiron
+# must be before including omni part
+TARGET_BOOTANIMATION_SIZE := 1080p
+
+PRODUCT_NAME := omni_chiron
 PRODUCT_DEVICE := chiron
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Mi MIX 2
+PRODUCT_MODEL := MI MIX 2
 PRODUCT_MANUFACTURER := Xiaomi
 
+# Google client ID.
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="chiron" \
+    PRODUCT_NAME="chiron" \
     PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V9.5.4.0.ODEMIFA release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys
